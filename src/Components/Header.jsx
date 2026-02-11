1f1934fd53315df069b3f61d1f5724cd472f6375
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-11 16:01:49 +0200

    new classes

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import {global} from '../assets/index'
+const Header = () => {
+    return (
+        <div className='matter'>
+            <img src={global} alt="The global object" />
+            <p>my travel journal.</p>
+        </div>
+    );
+}
+
+export default Header;
