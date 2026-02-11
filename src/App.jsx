58f518c2f282d3a9e3c16257694e962459e5609c
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-11 17:49:41 +0200

    access array element with props

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,31 @@ import viteLogo from '/vite.svg'
 import Header from './Components/Header'
 import Entry from './Components/Entry'
 import './App.css'
-
+import Places from './Components/data.js'
 function App() {
   const [count, setCount] = useState(0)
+  const getData = Places.map((element, id) => {
+    return (
+      <Entry
+      id={element.id} img={{
+      src: element.img.src,
+      alt : element.img.alt}} 
+      title={element.title}
+      country={element.country} 
+      googleMapsLink={element.googleMapsLink}
+      dates={element.dates}
+      text={element.text} />
+    );
+  })
+  console.log(getData);
 
   return (
     <>
-     <div className='fulah'>
+      <div className='fulah'>
         <Header />
-        <Entry />
+        <div className='container'>
+          {getData}
+        </div>
       </div>
     </>
   )
